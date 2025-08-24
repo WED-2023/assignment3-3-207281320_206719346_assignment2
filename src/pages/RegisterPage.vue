16630376e71cfc7ccc96598e396aea168d7efe72
Netanel Mordakhay <mordakhay.netanel@gmail.com>
2025-08-24 16:31:38 +0300

    updated registerpage with registerform comp

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -14,138 +14,7 @@
       </div>
     </template>
     <template #right>
-      <h2>Register</h2>
-      <b-form @submit.prevent="register">
-        <!-- Username -->
-        <b-form-group label="Username" label-for="username">
-          <b-form-input
-            id="username"
-            v-model="state.username"
-            :state="getValidationState(v$.username)"
-          />
-          <b-form-invalid-feedback v-if="v$.username.$error">
-            <div v-if="!v$.username.required">Username is required.</div>
-            <div v-else-if="!v$.username.minLength || !v$.username.maxLength">
-              Username must be 3–8 characters.
-            </div>
-            <div v-else-if="!v$.username.alpha">
-              Username must contain only letters.
-            </div>
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <!-- First Name -->
-        <b-form-group label="First Name" label-for="firstname">
-          <b-form-input
-            id="firstname"
-            v-model="state.firstname"
-            :state="getValidationState(v$.firstname)"
-          />
-          <b-form-invalid-feedback v-if="v$.firstname.$error">
-            First name is required.
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <!-- Last Name -->
-        <b-form-group label="Last Name" label-for="lastname">
-          <b-form-input
-            id="lastname"
-            v-model="state.lastname"
-            :state="getValidationState(v$.lastname)"
-          />
-          <b-form-invalid-feedback v-if="v$.lastname.$error">
-            Last name is required.
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <!-- Email -->
-        <b-form-group label="Email" label-for="email">
-          <b-form-input
-            id="email"
-            type="email"
-            v-model="state.email"
-            :state="getValidationState(v$.email)"
-          />
-          <b-form-invalid-feedback v-if="v$.email.$error">
-            <div v-if="!v$.email.required">Email is required.</div>
-            <div v-else-if="!v$.email.email">
-              Please enter a valid email address.
-            </div>
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <!-- Country -->
-        <b-form-group label="Country" label-for="country">
-          <b-form-select
-            id="country"
-            v-model="state.country"
-            :options="countries"
-            :state="getValidationState(v$.country)"
-          />
-          <b-form-invalid-feedback v-if="v$.country.$error">
-            Country is required.
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <!-- Password -->
-        <b-form-group label="Password" label-for="password">
-          <b-form-input
-            id="password"
-            type="password"
-            v-model="state.password"
-            :state="getValidationState(v$.password)"
-          />
-          <b-form-invalid-feedback v-if="v$.password.$error">
-            <div v-if="!v$.password.required">Password is required.</div>
-            <div v-else-if="!v$.password.minLength || !v$.password.maxLength">
-              Password must be 5–10 characters.
-            </div>
-            <div v-else-if="!v$.password.hasNumber">
-              Password must include at least one number.
-            </div>
-            <div v-else-if="!v$.password.hasSpecial">
-              Password must include at least one special character.
-            </div>
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <!-- Confirm Password -->
-        <b-form-group label="Confirm Password" label-for="confirmedPassword">
-          <b-form-input
-            id="confirmedPassword"
-            type="password"
-            v-model="state.confirmedPassword"
-            :state="getValidationState(v$.confirmedPassword)"
-          />
-          <b-form-invalid-feedback v-if="v$.confirmedPassword.$error">
-            <div v-if="!v$.confirmedPassword.required">
-              Confirmation is required.
-            </div>
-            <div v-else-if="!v$.confirmedPassword.sameAsPassword">
-              Passwords do not match.
-            </div>
-          </b-form-invalid-feedback>
-        </b-form-group>
-
-        <b-button type="submit" variant="success" class="w-100"
-          >Register</b-button
-        >
-
-        <b-alert
-          variant="danger"
-          class="mt-3"
-          dismissible
-          v-if="state.submitError"
-          show
-        >
-          Registration failed: {{ state.submitError }}
-        </b-alert>
-
-        <div class="mt-2">
-          Already have an account?
-          <router-link to="/login">Login here</router-link>
-        </div>
-      </b-form>
+      <RegisterForm />
     </template>
   </TwoColumnLayout>
 </template>
@@ -162,10 +31,11 @@ import {
 } from "@vuelidate/validators";
 import rawCountries from "../assets/countries";
 import TwoColumnLayout from "@/layouts/TwoColumnLayout.vue";
+import RegisterForm from "@/components/forms/RegisterForm.vue";
 
 export default {
   name: "RegisterPage",
-  components: { TwoColumnLayout },
+  components: { TwoColumnLayout, RegisterForm },
   setup() {
     const state = reactive({
       username: "",
